3560a2f5023a046b8fd24255888b7c614e96ac5e
tania-macheieva <macheieva.tetiana@gmail.com>
2025-12-07 19:05:33 +0200

    chore: clean env files and fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+const mode = process.env.NODE_ENV || 'development';
+dotenv.config({ path: `.env.${mode}` });
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL,
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       },
